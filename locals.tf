3b2236283211aeb827f5ebb35acb870fe03ef484
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2022-05-12 14:37:16 +0200

    Fixed get user group ids

--- locals.tf
+++ locals.tf
@@ -1,5 +1,7 @@
 locals {
   parameter_group_name = var.create_parameter_group ? module.parameter_group.id : var.replication_group_parameter_group_name
   subnet_group_name    = var.create_subnet_group ? module.subnet_group.name : var.replication_group_subnet_group_name
-  user_group_ids       = length(keys(var.user_groups)) > 0 ? module.user_group.*.id : var.replication_group_user_group_ids
+
+  group_ids      = [for k, v in module.user_group : v.id]
+  user_group_ids = length(keys(var.user_groups)) > 0 ? local.group_ids : var.replication_group_user_group_ids
 }
